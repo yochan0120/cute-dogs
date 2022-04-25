5925de84ed26af31dc32e0224f0626300b9fd48e
yo-chan <whitesnow.wave@gmail.com>
2022-04-25 11:26:56 +0900

    Initial Commit

---
+++ src/main/java/jp/co/cutedogs/CuteDogsApplication.java
@@ -0,0 +1,13 @@
+package jp.co.cutedogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CuteDogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CuteDogsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/cutedogs/CuteDogsApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.cutedogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CuteDogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
